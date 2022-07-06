750a861533e624c47b225add89e54594c962b99c
mustafa <yafess229@gmail.com>
2022-07-06 14:11:10 +0300

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaCalendarTimes} from "react-icons/fa";
+import { BsGenderFemale} from "react-icons/bs";
 import {AiFillMail} from "react-icons/ai";
 import {BiMapPin} from "react-icons/bi"
 
 const Card = ({ user }) => {
+
+  
 
 
 
@@ -12,7 +14,7 @@ const Card = ({ user }) => {
     picture: { large },
     name: { title, first, last },
     dob: { age },
-    gender: { gender },
+    gender,
     location: { city, country },
     email
   } = user
@@ -36,25 +38,19 @@ const Card = ({ user }) => {
 
 
         <h2>{age}</h2>
-        <h2>{gender}</h2>
-        <h2>{`${city}/${country}`}</h2>
-        <h2>{email}</h2>
+
+
+        <h2>{gender}
+        <BsGenderFemale size={20} /></h2>
+
+
+        <h2>{`${city}/${country}`}
+        <BiMapPin size={20}/></h2>
+
+
+        <h2>{email}
+        <AiFillMail size={20}/></h2>
       
-
-        <div className='d-flex justify-content-evenly mt-4 mb-4'>
-          <button>
-          <FaCalendarTimes size={20} />
-          </button>
-
-          <button>
-            <AiFillMail size={20}/>
-          </button>
-
-          <button>
-            <BiMapPin size={20}/>
-          </button>
-        
-        </div>
          
         </div>
 
